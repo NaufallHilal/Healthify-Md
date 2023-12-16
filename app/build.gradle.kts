cf9475178bb5946e5caaf587a42e695228bc7492
bersa96 <bersa.sanjaya@gmail.com>
2023-12-16 19:44:29 +0800

    UpdateData

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     testImplementation("junit:junit:4.13.2")
-    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.05.01"))
